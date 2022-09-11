ee5d41bd71abaaae5cac6511219202b3b0e8d4ee
Pankaj-Kumar-Das <pankajds12345@gmail.com>
2022-09-11 19:26:21 +0600

    Online Library System.With Database, using MYSQL as database server.

--- Library_Management_System_With_Database/library_management_system.py
+++ Library_Management_System_With_Database/library_management_system.py
@@ -2,5 +2,5 @@
 Author : Pankaj Kumar Das
 Start Date : 14/05/2022
 Purpose : Build a Console baseLibrary Management System with Database and using MYSQL as Database server.
-Problem Name : SQL_CURD
+Project Name : Library Management System
 """
