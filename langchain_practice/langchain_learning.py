967aba7589f2650c5a7e303b1cb5d722078610e2
Pankaj Kumar Das <71777467+PankajSP2019@users.noreply.github.com>
2023-11-05 19:36:18 +0600

    Delete langchain_practice/langchain_learning.py

--- langchain_practice/langchain_learning.py
+++
@@ -1,9 +0,0 @@
-import openai
-import os
-from langchain.llms import OpenAI
-
-OPENAI_API_KEY = ""
-
-
-llm = OpenAI(openai_api_key=OPENAI_API_KEY, temperature=0.3)
-print(llm.predict("what is python?"))
