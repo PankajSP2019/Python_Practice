913c35d1b825ba2f5bc69a5d5221c8f944e773a8
Pankaj <pankajds12345@gmail.com>
2023-11-06 20:31:37 +0600

    Testing

--- langchain_practice/langchain_learning.py
+++ langchain_practice/langchain_learning.py
@@ -2,8 +2,8 @@ import openai
 import os
 from langchain.llms import OpenAI
 
-OPENAI_API_KEY = ""
-
+with open("C:\\Users\\ASUS\\Desktop\\office\\OpenAI API Key For me.txt") as f:
+    OPENAI_API_KEY = f.readlines()[0]
 
 llm = OpenAI(openai_api_key=OPENAI_API_KEY, temperature=0.3)
-print(llm.predict("what is python?"))
+print(llm.predict("How conversation buffer memory works in langchain, tell me in short with an example?"))
